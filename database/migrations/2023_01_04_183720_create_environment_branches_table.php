e3088989d1f461cdd422f38c7c64ac2ca02113bf
sandraaro <sandra16bb@gmail.com>
2023-04-26 15:36:56 -0400

    Agregando las migraciones

---
+++ database/migrations/2023_01_04_183720_create_environment_branches_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('environment_branches', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('environment_id')->constrained();
+            $table->foreignId('branch_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enviroment_branches');
+    }
+};
